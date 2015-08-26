644b6a2a177dc091c0f8c103bf3d5fe1e29b15ce
evanharris <evan.tyler.harris@gmail.com>
2015-08-25 23:16:29 -0700

    adding IOC with manager pattern

--- alt/alt_updater.py
+++ alt/alt_updater.py
@@ -1,23 +1,23 @@
 __author__ = 'whiteboarddev'
 
 import sys
-
 from util import config
-from data_access import firebase_client, node_repository
-import model.alt_node
 from model import alt_node_factory
-
+import logging
+from util import LifecycleManager
 def alt_update():
-    alt_config = config.Config(sys.argv[1])
-    node_repo = node_repository.Node_Repository(firebase_client.Firebase_Client(alt_config.get_firebase_url(), alt_config.get_firebase_secret()))
-
-    stored_node = node_repo.get_node(alt_config.get_node_name())
-    actual_node = alt_node_factory.create_alt_node(alt_config.get_node_name())
+    #bootstrap application by setting configs
+    LifecycleManager.register_config(config.Config(sys.argv[1]))
+    config_data = LifecycleManager.get_config()
+    node_repo = LifecycleManager.get_node_repository()
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+    stored_node = node_repo.get_node(config_data.get_node_name())
+    actual_node = alt_node_factory.create_alt_node(config_data.get_node_name())
     if stored_node is None or not stored_node.equals(actual_node):
-        print "Detected node differences"
+        logging.info("Detected node differences")
         node_repo.update_node(actual_node)
     else:
-        print "Skipping update, node up to date"
+        logging.info("Skipping update, node up to date")
 
 
 alt_update()
