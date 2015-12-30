e4cae2a39bfe78c7fba1ee1b825a1cd911ff83e0
evanharris <evan.tyler.harris@gmail.com>
2015-12-29 19:24:11 -0800

    updating installation script

--- installation/install.sh
+++ installation/install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-SOURCE_LOCATION=https://github.com/WhiteBoardDev/address-location-tool/archive/0.1.tar.gz
+SOURCE_LOCATION=https://codeload.github.com/WhiteBoardDev/address-location-tool/tar.gz/0.1
 
-BIN_DIR=/opt/address-location-tool
+BIN_DIR=/opt/alt
+CONF_DIR=/etc/alt/conf
+
 
 curl -o $TMPDIR/address-location-tool.tar.gz $SOURCE_LOCATION
 
 test -d $BIN_DIR && echo "Installation Detected, auto removing"
-if [ -d $BIN_DIR]; then
-  rmdir -rf $BIN_DIR
+if [ -d $BIN_DIR ]; then
+  rm -rf $BIN_DIR
 fi
 
 mkdir $BIN_DIR
-tar -xvzf $TMPDIR/address-location-tool.tar.gz -C $BIN_DIR
+tar -xvzf $TMPDIR/address-location-tool.tar.gz -C $BIN_DIR  --strip-components=1
